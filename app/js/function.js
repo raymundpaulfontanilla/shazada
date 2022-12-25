a4da9d8a10fab0c31975862fe245f704a842b7e9
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2022-12-25 20:30:08 +0800

    footer implementation

--- app/js/function.js
+++ app/js/function.js
@@ -34,7 +34,7 @@ function getJSONDataForContainer(urlData) {
          <a href="/template/men-categories.html">
           <div class='male-content'>
             <h1 class='title'>${imageContain.title}</h1>
-            <button class='subtitle'>${imageContain.subtitle}</button>
+            <button class='subtitle rounded-pill'>${imageContain.subtitle}</button>
           </div>
           </a>
         </div>
@@ -43,7 +43,7 @@ function getJSONDataForContainer(urlData) {
           <a href="/template/women-categories.html">
           <div class='women-content'>
             <h1 class='title'>${imageContain.title2}</h1>
-            <button class="subtitle">${imageContain.subtitle}</button>
+            <button class="subtitle rounded-pill">${imageContain.subtitle}</button>
           </div>
           </a>
         </div>
