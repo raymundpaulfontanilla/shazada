2d1287e0ef186eec69520b4f085f3e32b6206868
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2022-12-29 06:34:31 +0800

    Implement JS ES6 Destructuring Array to avoid DRY

--- app/js/function.js
+++ app/js/function.js
@@ -4,24 +4,25 @@ function getJSONDataForContainer(urlData) {
     .then((json) => {
       let imageContainer = json.slice(0);
       imageContainer.map((imageContain) => {
+        let { mensUrl, title, subtitle, womensUrl, title2 } = imageContain;
         document.getElementById(
           "men-women-container"
         ).innerHTML += `<div class="row">
         <div class="col-lg-6">
-         <img src=${imageContain.mensUrl} class="image-container" width="500">
+         <img src=${mensUrl} class="image-container" width="500">
          <a href="/template/men-categories.html">
           <div class='male-content'>
-            <h1 class='title'>${imageContain.title}</h1>
+            <h1 class='title'>${title}</h1>
             <button class='subtitle rounded-pill'>${imageContain.subtitle}</button>
           </div>
           </a>
         </div>
         <div class="col-lg-6">
-          <img src=${imageContain.womensUrl} class="image-container" width="460">
+          <img src=${womensUrl} class="image-container" width="460">
           <a href="/template/women-categories.html">
           <div class='women-content'>
-            <h1 class='title'>${imageContain.title2}</h1>
-            <button class="subtitle rounded-pill">${imageContain.subtitle}</button>
+            <h1 class='title'>${title2}</h1>
+            <button class="subtitle rounded-pill">${subtitle}</button>
           </div>
           </a>
         </div>
@@ -35,16 +36,17 @@ function getJSONData(urlData) {
     .then((json) => {
       let images = json.slice(0);
       images.map((image) => {
+        let { url, name, price } = image;
         document.getElementById("container").innerHTML += `
       <figure class="container">
-        <img src=${image.url}
+        <img src=${url}
           alt="sample94" />
         <figcaption>
           <h3 onclick="addToCart()">Add to cart</h3>
         </figcaption>
         <div class="footer">
-        <span class="card-title">${image.name}</span>
-        <span class="card-text" style="color:orange;">₱${image.price}</span>
+        <span class="card-title">${name}</span>
+        <span class="card-text" style="color:orange;">₱${price}</span>
         </div>
       </figure>`;
       });
