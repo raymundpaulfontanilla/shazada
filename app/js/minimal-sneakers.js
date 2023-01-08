b4e5919aa7838d5024a064b88703c07faf3d8236
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2023-01-08 11:48:44 +0800

    added file

---
+++ app/js/minimal-sneakers.js
@@ -0,0 +1,50 @@
+let shopItemsData = [
+  {
+    id: "a",
+    img: "https://mensflair.com/wp-content/uploads/2022/06/MinimalistSneakerBrands15-768x960.jpg",
+    name: "CQP Tarmac Light",
+    price: 2000,
+  },
+  {
+    id: "b",
+    img: "https://mensflair.com/wp-content/uploads/2022/06/MinimalistSneakerBrands10-768x960.jpg",
+    name: "Aurélien Bayside",
+    price: 3000,
+  },
+  {
+    id: "c",
+    img: "https://mensflair.com/wp-content/uploads/2022/06/MinimalistSneakerBrands3-768x960.jpg",
+    name: "JAK Royal Black",
+    price: 4000,
+  },
+  {
+    id: "d",
+    img: "https://mensflair.com/wp-content/uploads/2022/07/MinimalistSneakerBrands32-768x960.jpg",
+    name: "Oliver Cabell Low",
+    price: 5000,
+  },
+  {
+    id: "e",
+    img: "https://images.opumo.com/storage/product/cariuma-off-white-canvas-catiba-pro-sneakers.jpg?w=541&h=731",
+    name: "Cariuma",
+    price: 7000,
+  },
+  {
+    id: "f",
+    img: "https://images.opumo.com/storage/product/velasca-black-smooth-leather-moros-loafers1.jpg?w=541&h=731",
+    name: "Velasca",
+    price: 8000,
+  },
+  {
+    id: "g",
+    img: "https://images.opumo.com/storage/product/uniform-standard-triple-khaki-nubuck-series-1-sneakers1.jpg?w=541&h=731",
+    name: "Uniform Stndrd",
+    price: 9000,
+  },
+  {
+    id: "h",
+    img: "https://images.opumo.com/storage/product/north-89-universe-breathable-no-2-sneakers1.jpg?w=541&h=731",
+    name: "North-89",
+    price: 10000,
+  },
+];
